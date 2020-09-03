7a58075bfa3fdd8afa7701ab760cca6c879fad97
Yuxiang Sun <sunyuxiangyx@gmail.com>
2020-09-03 03:07:58 -0400

    Add class Unit Test

---
+++ Unit Test/unitTest.h
@@ -0,0 +1,15 @@
+#include <functional>
+#include <iostream>
+template <class Ret, class... Args>
+class UnitTest {
+    string input;
+    string output;
+    function<Ret(Args...)> func;
+    public:
+    bool cmp() {
+
+    }
+    
+
+
+}
