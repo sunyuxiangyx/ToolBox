10bc2f9ae6d9136a61bb2f055d7011c604ae6386
Yuxiang Sun <sunyuxiangyx@gmail.com>
2020-09-03 15:07:13 -0400

    Move to .h file

--- Unit Test/main.cc
+++ Unit Test/main.cc
@@ -1,72 +1,28 @@
-#include <functional>
-#include <iostream>
-#include <sstream>
-#include <tuple>
-#include <algorithm>
+#include "unitTest.h"
+#include <vector>
 using namespace std;
 
-#include <functional>
-#include <iostream>
-template <class Ret, class... Args>
-class UnitTest {
-    public:
-    function<Ret(Args...)> func;
-    tuple<Args...> arguments;
-    Ret rev;
-    string input;
-    string output;
-    string name;
-    string comment;
-    UnitTest(function<Ret(Args...)> func, tuple<Args...> args, Ret rev): func{func}, arguments{args}, rev{rev} {} 
-    bool check() const {
-
-        istringstream iss{input};
-        streambuf* cinbuf = std::cin.rdbuf();
-        cin.rdbuf(iss.rdbuf());
-
-        ostringstream oss;
-        streambuf* coutbuf = std::cout.rdbuf();
-        cout.rdbuf(oss.rdbuf());
-
-        Ret actullyRev = apply(func, arguments);
-
-        std::cin.rdbuf(cinbuf);
-        std::cout.rdbuf(coutbuf);
-
-
-        if (rev == actullyRev and oss.str() == output) {
-            cerr << "Success: " << name << endl;
-            return true;
-        } else {
-            cerr << "Fail: " << name << endl;
-            cerr << "Arguments:";
-            apply([](const auto&... args) {((cout << " "<< args), ...);}, arguments);
-            cerr << endl;
-            cerr << "Input: " << input << endl;
-            cerr << "Expected Output: " << output << endl;
-            cerr << "Actual Output: " << oss.str() << endl;
-            cerr << "Comment: " << comment << endl;
-            return false;
-        }
-    }
-};
-
-int foo(int b, bool c, string d) {
-    int x;
-    cin >> x;
-    cout << x * x;
-    if (c) {
-        cerr << d << endl;
-    } else {
-        return b;
-    }
-    return d.length();
+int foo(string domain) {
+    string username;
+    cin >> username;
+    cout << username << '@' << domain << endl;
+    return username.length() + domain.length() + 1;
 }
 
 int main() {
-    UnitTest<int, int, bool, string> f{foo, {4, false, "str"}, 4};
-    f.name = "Basic Test";
-    f.comment = "failure comment";
-    f.output = "16";
-    f.check();
+    vector<UnitTest<int, string>> tests;
+    tests.push_back({foo, {"gmail.com"}, 4});
+    tests.back().input = "Francis";
+    tests.back().output = "Francis@gmail.com\n";
+    tests.back().rev = 17;
+    tests.back().name = "gmail test";
+
+    tests.push_back({foo, {"hotmail.com"}, 4});
+    tests.back().input = "Francis";
+    tests.back().output = "Francis@hotmail.com\n";
+    tests.back().rev = 19;
+    tests.back().name = "hotmail test";
+    for (const auto& test: tests) {
+        test.check();
+    }
 }
--- Unit Test/unitTest.h
+++ Unit Test/unitTest.h
@@ -1,15 +1,67 @@
+#ifndef __UNITTEST_H__
+#define __UNITTEST_H__
 #include <functional>
 #include <iostream>
-template <class Ret, class... Args>
-class UnitTest {
-    string input;
-    string output;
-    function<Ret(Args...)> func;
-    public:
-    bool cmp() {
+#include <sstream>
+#include <tuple>
 
+struct RedirectIOStream {
+    std::ios& stream;
+    std::streambuf* buf;
+    std::stringstream ss;
+    RedirectIOStream(std::ios& stream, std::string s=""): stream{stream}, buf{stream.rdbuf()}, ss{s} {
+        stream.rdbuf(ss.rdbuf());
     }
+    void reset() {
+        stream.rdbuf(buf);
+    }
+    ~RedirectIOStream() {
+        reset();
+    }
+};
+
+template <class Ret, class... Args>
+struct UnitTest {
+    std::function<Ret(Args...)> func;
+    std::tuple<Args...> arguments;
+    Ret rev;
+    std::string input;
+    std::string output;
+    std::string name;
+    std::string comment;
+    bool hiddenResult = false;
+    UnitTest(std::function<Ret(Args...)> func, std::tuple<Args...> args, Ret rev): func{func}, arguments{args}, rev{rev} {} 
+    bool check() const {
+        using std::cerr;
+        using std::endl;
+        RedirectIOStream in{std::cin, input};
+        RedirectIOStream out{std::cout};
     
+        Ret actullyRev = apply(func, arguments);
+
+        in.reset();
+        out.reset();
+
+        if (rev == actullyRev and out.ss.str() == output) {
+            cerr << "Success: " << name << endl;
+            return true;
+        } else {
+            cerr << "Fail: " << name << endl;
+            if (!hiddenResult) {
+                cerr << "Arguments:";
+                apply([](const auto&... args) {((cerr << " "<< args), ...);}, arguments);
+                cerr << endl;
+                cerr << "Input: " << input << endl;
+                cerr << "Expected Return : " << rev << endl;
+                cerr << "Actual Return: " << actullyRev << endl;
+                cerr << "Expected Output: " << output << endl;
+                cerr << "Actual Output: " << out.ss.str() << endl;
+            }
+            cerr << "Comment: " << comment << endl;
+            return false;
+        }
+    }
+};
 
 
-}
+#endif
